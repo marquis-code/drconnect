e0670843237d31d70d54763da40b7c75007db940
marquis-code <abahmarquis@gmail.com>
2025-10-31 10:40:23 +0100

    deployed backend and fixed uild error

--- dist/config/database.config.d.ts
+++
@@ -1,3 +0,0 @@
-import type { MongooseModuleOptions } from "@nestjs/mongoose";
-export declare const mongooseConfig: () => MongooseModuleOptions;
-//# sourceMappingURL=database.config.d.ts.map
--- dist/config/database.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongooseConfig = void 0;
-const mongooseConfig = () => ({
-    uri: process.env.MONGODB_URI
-});
-exports.mongooseConfig = mongooseConfig;
-//# sourceMappingURL=database.config.js.map
